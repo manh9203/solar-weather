66f086dfac1803bdd474c156d0ff56dc7c1701f4
RYZEXY <alicepleung@gmail.com>
2023-06-26 09:33:59 -0400

    installed packages

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express"; // importing the module
+import * as utilities from "./utils/functions";
+import bodyParser from "body-parser";
+
+import cors from "cors";
+
+
+
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
+
+const app = express(); // creating an Express app
+const { PORT = 3000 } = process.env;
+app.use(bodyParser.json()).use(cors());
+
+app.get("/", (request, response) => response.send("Hello World!"));
+
+

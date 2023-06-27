9123dadc3da99d6a6906f38505f70c6c7d672eb9
Andy Chang <andychang268@gmail.com>
2023-06-27 15:09:03 -0700

    Improved on SetDate. Don't really know how to test it, as it is a library component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react'
+import { useState } from "react";
 import SolarEventList from "./components/SolarEventList";
-import './App.css'
+import "./App.css";
 import PropTypes from "prop-types";
-function App(props) {
+import SetDate from "./components/SetDate";
+
+function App({ name, testMode }) {
+
+  const tempCall = () => console.log("Calling API!")
+
+  let x = new Date();
+  x.setDate(x.getDate() - 7);
+  const [startDate, setStartDate] = useState(x);
+  const [endDate, setEndDate] = useState(new Date());
 
   return (
     <>
-    <h1>{props.name}</h1>
-    <SolarEventList />
-      
+      <h1>{name}</h1>
+      <SetDate
+        startDate={startDate}
+        setStartDate={setStartDate}
+        endDate={endDate}
+        setEndDate={setEndDate}
+        callAPI={tempCall}
+      />
+      <SolarEventList />
+      {!testMode ? null : (
+        <div>
+          <h6>Start Date: {startDate ? startDate.toDateString() : null}</h6>
+          <h6>End Date: {endDate ? endDate.toDateString() : null}</h6>
+        </div>
+      )}
     </>
-  )
+  );
 }
+App.propTypes = {
+  name: PropTypes.string,
+  testMode: PropTypes.bool,
+};
 
-export default App
+export default App;
--- src/components/SetDate.jsx
+++ src/components/SetDate.jsx
@@ -1,57 +1,64 @@
-import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import PropTypes from "prop-types";
 
+function SetDate({ startDate, setStartDate, endDate, setEndDate, callAPI }) {
+  function handleStartDateChange(date) {
+    const actual = new Date(date);
+    actual.setDate(actual.getDate() + 7);
+    setEndDate(actual);
+    setStartDate(date);
+  }
 
-const SetDate = () => {
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
+  function handleEndDateChange(date) {
+    const actual = new Date(date);
+    actual.setDate(actual.getDate() - 7);
+    setStartDate(actual);
+    setEndDate(date);
+  }
 
-  function handleStartDateChange(event){
-    setStartDate(event.target.value);
-  };
-
-  function handleEndDateChange(event) {
-    setEndDate(event.target.value);
-  };
-
-  function handleSubmit(event){
+  function handleSubmit(event) {
     event.preventDefault();
     // call the DONKI api with start and end dates over here
-
     //for testing
-    console.log('Start Date:', startDate);
-    console.log('End Date:', endDate);
-
-
-    //reset
-    setStartDate(null)
-    setEndDate(null);
-  };
+    console.log("Start Date:", startDate);
+    console.log("End Date:", endDate);
+    callAPI();
+  }
 
   return (
     <form onSubmit={handleSubmit}>
+      <legend>Display any 7 days of Space Weather Information!</legend>
       <label htmlFor="startDate">Start Date:</label>
       <DatePicker
+        data-testid="start-date-picker"
         id="startDate"
         selected={startDate}
-        onChange={handleStartDateChange}
+        onChange={(date) => handleStartDateChange(date)}
         dateFormat="MM/dd/yyyy"
         required
       />
 
       <label htmlFor="endDate">End Date:</label>
       <DatePicker
+      data-testid="end-date-picker"
         id="endDate"
         selected={endDate}
-        onChange={handleEndDateChange}
+        onChange={(date) => handleEndDateChange(date)}
         dateFormat="MM/dd/yyyy"
+        data-test
         required
       />
 
       <button type="submit">Submit</button>
     </form>
   );
+}
+SetDate.propTypes = {
+  startDate: PropTypes.object,
+  endDate: PropTypes.object,
+  setStartDate: PropTypes.func,
+  setEndDate: PropTypes.func,
 };
 
 export default SetDate;

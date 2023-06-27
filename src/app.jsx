d911fd44842283accea5c62bc12187d83550429b
RYZEXY <alicepleung@gmail.com>
2023-06-27 13:04:00 -0400

    added empty files and calender date input

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import SolarEventList from "./components/SolarEventList";
+import './App.css'
+
+function App(props) {
+
+  return (
+    <>
+    <h1>{props.name}</h1>
+    <SolarEventList />
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SolarEventList.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+function SolarEventList(){
+    const [date, setDate] = useState([]);
+    useEffect(() =>{
+            
+    });
+    const[solarEvent, setSolarEvent] = useState([]);
+
+
+}

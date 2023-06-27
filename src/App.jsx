2e59ae474ff99e9d1c0060449dbe86fec9983579
Andy Chang <andychang268@gmail.com>
2023-06-26 19:42:48 -0700

    Initialized Vite and added .gitignore so it works - Added some basic timekeeping features to the app, it's going to need a   LOT more work later though.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import SunLogo from "./assets/sun-svgrepo-com.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [date, setDate] = useState(new Date());
+  const weekAgo = structuredClone(date);
+  weekAgo.setDate(weekAgo.getDate() - 7);
+
+  useEffect( () => {
+    const interval = setInterval( () => {
+      setDate(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [])
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={SunLogo} alt="sun" />
+        <h1>Space Weather</h1>
+        <h3>Time now is: {date.toUTCString()}</h3>
+        <h3>Time a week ago is: {weekAgo.toUTCString()}</h3>
       </div>
-      <h1>Manulife Team 292</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

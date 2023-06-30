db3e65ab75c98571de87f773ed90add4d80df7b5
Manh <manh9203@gmail.com>
2023-06-30 15:14:07 +0700

    deploy vite app

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/solar-weather/"
+  base: "/solar-weather/",
   plugins: [react()],
   server: {
     port: 3000
